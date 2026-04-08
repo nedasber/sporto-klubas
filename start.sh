1c90b2fe39b1db12019aa010a5eb429704e98b26
nedas <147548576+nedasber@users.noreply.github.com>
2026-04-08 16:59:45 +0300

    Update settings for database URL

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "STARTING APP"
 python manage.py shell -c "from gym.seed_users import create_test_users; create_test_users()"
-gunicorn config.wsgi:application
+echo "SEED DONE"
+exec gunicorn config.wsgi:application
