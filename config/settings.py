4b68e7d255ecdc29672a24784c6d2e267231a2d5
nedas <147548576+nedasber@users.noreply.github.com>
2026-04-08 18:35:17 +0300

    Fix admin theme

--- config/settings.py
+++ config/settings.py
@@ -183,7 +183,7 @@ JAZZMIN_UI_TWEAKS = {
     "sidebar_nav_compact_style": False,
     "sidebar_nav_legacy_style": False,
     "sidebar_nav_flat_style": False,
-    "theme": "darkly",
+    "theme": "default",
     "button_classes": {
         "primary": "btn-primary",
         "secondary": "btn-secondary",
